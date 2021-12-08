ddec0acbc371c869b4c50fdd224396734a94f1f6
Vika <Vikasatta1998@gmail.com>
2021-12-08 22:55:05 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type UserType = {
+    userId:string,
+    photo: string,
+    userName: string
+    message: string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [messageText, setMessagetext] = useState<any>('')
+    let [ws, setWs] = useState<any>(null)
+    let [users, setUsers] = useState<Array<UserType>>([])
+
+    useEffect(() => {
+        let localWS = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+        localWS.onmessage = (messageEvent) => {
+
+            let messages = JSON.parse(messageEvent.data)
+            console.log(messages)
+            setUsers( messages)
+        }
+        setWs(localWS)
+    }, [])
+
+    let onMessageChange = (e: any) => {
+        setMessagetext(e.currentTarget.value)
+    }
+    let sendMessage = () => {
+        ws.send(messageText)
+
+    }
+
+
+    return (
+        <div className="Chat">
+            <div className={'messages'}>
+                {users.map(u => <div className={'message'} >
+                    <img src={u.photo}/><b>{u.userName}</b><span>{u.message}</span>
+                </div>)}
+            </div>
+            <div className={'footer'}>
+                <textarea onChange={onMessageChange}>{messageText}</textarea>
+                <button onClick={sendMessage}>Send</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
